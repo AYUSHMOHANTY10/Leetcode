0360f28bb19c2202692edc142190f03ab7b96796
AYUSH MOHANTY <92939434+AYUSHMOHANTY10@users.noreply.github.com>
2023-06-26 22:31:40 +0530

    Create Sol.py

---
+++ 121. Best Time to Buy and Sell Stock/Sol.py
@@ -0,0 +1,24 @@
+'''class Solution:
+    def maxProfit(self,prices):
+        left = 0
+        right = 1
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left]
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit'''
+class Solution(object):
+    def maxProfit(self, prices):
+        profit = 0
+        buy = prices[0]
+        for sell in prices[1:]:
+            if sell > buy:
+                profit = max(profit, sell - buy)
+            else:
+                buy = sell
+        
+        return profit
